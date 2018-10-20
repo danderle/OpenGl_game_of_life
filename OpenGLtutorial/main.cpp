822227a7f3845996ee0c65d9e94f96e75928ab05
Dennis Anderle <danderle@denniss-imac.fritz.box>
2018-10-20 21:34:20 +0200

    Initial Commit

---
+++ OpenGLtutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLtutorial
+//
+//  Created by Dennis Anderle on 20.10.18.
+//  Copyright © 2018 Dennis Anderle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
